2fde81622d5e82e1ca4b5ad900c4fd234159df94
Fagner Lopes Carvalho <fagnerlc@icloud.com>
2022-11-30 23:13:53 -0300

    criando funcao q calcula idade

--- lib/49_falc/datas/49_datas_funcoes_prontas.dart
+++ lib/49_falc/datas/49_datas_funcoes_prontas.dart
@@ -1,5 +1,7 @@
 void main(List<String> args) {
   print(periodicidade(dataEvento: DateTime.now(), ciclos: 7, periodicidade: 30));
+  print(calculaIdade(DateTime(1987, 01, 15)));
+  print(calculaIdade(DateTime(2021, 12, 01)));
 }
 
 List<DateTime> periodicidade({
@@ -16,4 +18,21 @@ List<DateTime> periodicidade({
     dataCalc = dataCalc.add(Duration(days: periodicidade));
   }
   return listaDatas;
+}
+
+calculaIdade(DateTime dataNascimento) {
+  DateTime dataCorente = DateTime.now();
+  int idade = dataCorente.year - dataNascimento.year;
+  int mes1 = dataCorente.month;
+  int mes2 = dataNascimento.month;
+  if (mes2 > mes1) {
+    idade--;
+  } else if (mes1 == mes2) {
+    int day1 = dataCorente.day;
+    int day2 = dataNascimento.day;
+    if (day2 > day1) {
+      idade--;
+    }
+  }
+  return idade;
 }
